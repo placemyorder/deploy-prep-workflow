fc1e724dab698196a7c6e02e75b8414a747f73be
Madhav <mshenoy83@gmail.com>
2023-12-10 01:54:03 +1100

    update powershell script

---
+++ autoincrementversion.ps1
@@ -0,0 +1,29 @@
+param(
+    [parameter(Mandatory=$true)]
+    [String]$branch,
+    [parameter(Mandatory=$true)]
+    [String]$versionFile
+)
+$fileVersion = [version](Get-Content $versionFile)
+$stringArray = $branch.ToString().Split("/")
+$major = $fileVersion.Major
+$minor = $fileVersion.Minor
+$patch = $fileVersion.Build
+
+if($stringArray[0] -eq "release")
+{
+    $major = $major + 1
+}
+
+if($stringArray[0] -eq "feature")
+{
+    $minor = $minor + 1
+}
+
+if($stringArray[0] -eq "bug")
+{
+    $patch = $patch + 1
+}
+
+$version = "$major.$minor.$patch"
+Set-Content $versionFile $version
